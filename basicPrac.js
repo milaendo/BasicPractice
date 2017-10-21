1f35c9af8a6ceccf0442922a0de248dbb1111bcd
Mila Endo <milal.alim@gmail.com>
2017-10-20 20:41:00 -0700

    a few more

--- basicPrac.js
+++ basicPrac.js
@@ -7,3 +7,35 @@ function isGreaterThan( numberOne, numberTwo ){
   }
 }
 console.log(isGreaterThan(4,5))
+
+///////////////////////////////////////////////////////////////
+
+const volumeOfSphere = diameter => 
+(1/6) * Math.PI * diameter * diameter * diameter
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+
+///////////////////////////////////////////////////////////////
+
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+let stars = 'North Star'
+
+const myNightSky = () => { return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;}
+
+console.log(myNightSky())
+
+///////////////////////////////////////////////////////////////
+
+const visibleLightWaves = () => {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+    if (region === 'The Arctic') {
+      let lightWaves = 'Northern Lights';
+      console.log(lightWaves);  
+    }
+  console.log(lightWaves);
+};
+
+visibleLightWaves();
+
